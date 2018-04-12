fb397bf07fa1082182f893a164b7c47c7f2879ac
Roberto Lucha <roberto.lucha@uswitch.com>
2018-04-13 00:19:43 +0100

    Light in room, scene switcher

--- src/index.js
+++ src/index.js
@@ -1,48 +1,7 @@
-import {
-  Vector3,
-  DoubleSide,
-  Geometry,
-  Face3,
-  Mesh,
-  ShapeUtils,
-  MeshNormalMaterial
-} from 'three'
+// import facesScene from './scenes/faces'
 
-import {createScene} from './scene_setup'
+// facesScene()
 
-// Creating custom meshes
+import lights from './scenes/lights'
 
-// Scene
-const scene = createScene()
-
-var geometry = new Geometry()
-geometry.vertices.push(new Vector3(0, 0, 0))
-geometry.vertices.push(new Vector3(0, 100, 0))
-geometry.vertices.push(new Vector3(100, 0, 0))
-
-// You cannot have two same vertices or the geom triangulation won't work
-geometry.vertices.push(new Vector3(0, 0, 100))
-geometry.vertices.push(new Vector3(100, 100, 0))
-
-// for (var i = 0; i < 10000; i++) {
-//   var vertex = new Vector3()
-//   vertex.x = Math.random() * 1000 - 500
-//   vertex.y = Math.random() * 1000 - 500
-//   vertex.z = Math.random() * 1000 - 500
-//   geometry.vertices.push(vertex)
-// }
-const triangles = ShapeUtils.triangulateShape(geometry.vertices, [])
-
-triangles.forEach(t => {
-  geometry.faces.push(new Face3(t[0], t[1], t[2]))
-})
-// geometry.computeFaceNormals()
-// We need to compute the vertex normals for MeshNormalMaterial to work
-geometry.computeVertexNormals()
-
-// Double side to draw both sides of the face
-var object = new Mesh(geometry, new MeshNormalMaterial({side: DoubleSide}))
-scene.add(object)
-
-// TO read
-// What is a vertex normal and what relation has with face?
+lights()
---
+++ src/scenes/lights.js
@@ -0,0 +1,53 @@
+import {
+  Vector3,
+  DoubleSide,
+  Geometry,
+  Face3,
+  Mesh,
+  ShapeUtils,
+  MeshNormalMaterial,
+  SphereGeometry,
+  MeshBasicMaterial,
+  PointLight,
+  BackSide,
+  BoxGeometry,
+  MeshPhongMaterial
+} from 'three'
+
+import {createScene} from '../scene_setup'
+
+export default () => {
+// Scene
+  const scene = createScene()
+  const color = 0xffffff
+  const intensity = 1.5
+
+  var pointLight = new PointLight(color, intensity, 50)
+  pointLight.castShadow = true
+  pointLight.shadow.camera.near = 1
+  pointLight.shadow.camera.far = 60
+  pointLight.shadow.bias = -0.005 // reduces self-shadowing on double-sided objects
+
+  // helper to position the light
+  var geometry = new SphereGeometry(1, 12, 12)
+  var material = new MeshBasicMaterial({ color: color })
+  material.color.multiplyScalar(intensity)
+  var sphere = new Mesh(geometry, material)
+  pointLight.add(sphere)
+
+  var geometry2 = new BoxGeometry(30, 30, 30)
+  var material2 = new MeshPhongMaterial({
+    color: 0xff0000,
+    shininess: 10,
+    specular: 0x111111,
+    side: BackSide
+  })
+
+  var mesh = new Mesh(geometry2, material2)
+  mesh.position.y = 10
+  mesh.receiveShadow = true
+  scene.add(mesh)
+  scene.add(pointLight)
+// TO read
+// What is a vertex normal and what relation has with face?
+}
